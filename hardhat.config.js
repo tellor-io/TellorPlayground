41ec6e81a6d82149f681c930ea32a68640864065
brendaloya <l.loya.b@gmail.com>
2021-09-13 15:54:32 -0400

    updated hardhat config for pr

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,25 +24,25 @@ module.exports = {
     ]
   },
   networks: {
-    // hardhat: {
-    //   hardfork: process.env.CODE_COVERAGE ? "berlin" : "london",
-    //   initialBaseFeePerGas: 0,
-    //   accounts: {
-    //     mnemonic:
-    //       "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
-    //     count: 40,
-    //   },
-    //   forking: {
-    //     url: "https://eth-mainnet.alchemyapi.io/v2/7dW8KCqWwKa1vdaitq-SxmKfxWZ4yPG6"
-    //   },
-    //   allowUnlimitedContractSize: true
-    // },
-    rinkeby: {
-         url: `${process.env.NODE_URL_RINKEBY}`,
-         seeds: [process.env.PRIVATE_KEY],
-         gas: 10000000,
-         gasPrice: 40000000000
+    hardhat: {
+      hardfork: process.env.CODE_COVERAGE ? "berlin" : "london",
+      initialBaseFeePerGas: 0,
+      accounts: {
+        mnemonic:
+          "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
+        count: 40,
+      },
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/7dW8KCqWwKa1vdaitq-SxmKfxWZ4yPG6"
+      },
+      allowUnlimitedContractSize: true
     }//,
+    // rinkeby: {
+    //      url: `${process.env.NODE_URL_RINKEBY}`,
+    //      seeds: [process.env.PRIVATE_KEY],
+    //      gas: 10000000,
+    //      gasPrice: 40000000000
+    // }//,
       // mainnet: {
       //   url: `${process.env.NODE_URL_MAINNET}`,
       //   accounts: [process.env.PRIVATE_KEY],

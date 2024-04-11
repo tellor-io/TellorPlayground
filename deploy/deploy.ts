dabf05c5c86aa8ff7f9ae3a479cf702ae9bc5d00
brendaloya <l.loya.b@gmail.com>
2024-04-11 14:59:08 -0400

    zksync stuff

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,10 +5,18 @@ import { deployContract } from "./utils";
 // An example of a basic deploy script
 // It will deploy a contract to selected network
 // as well as verify it on Block Explorer if possible for the network
-export default async function () {
+async function deploy () {
   const contractArtifactName = "TellorPlayground";
   //const constructorArguments = ["2",5,6];
   //await deployContract(contractArtifactName, constructorArguments);
-  await deployContract(contractArtifactName);
+  console.log("before deployment")
+  await deployContract(contractArtifactName,[]);
   console.log("deployed")
 }
+
+deploy( )
+    .then(() => process.exit(0))
+    .catch(error => {
+	  console.error(error);
+	  process.exit(1);
+  });
